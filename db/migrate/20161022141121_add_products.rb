5ebe52e4fe3f6f47d146584e3998c168457e6221
Rail Sabirov <railich@bk.ru>
2016-11-10 21:16:34 +0300

    migration

---
+++ db/migrate/20161022141121_add_products.rb
@@ -0,0 +1,31 @@
+class AddProducts < ActiveRecord::Migration[5.0]
+  def change
+    Product.create :title => 'Hawaiian',
+                   :description => 'This is Hawaiian pizza',
+                   :price => 350,
+                   :size => 30,
+                   :is_spicy => false,
+                   :is_veg => false,
+                   :is_best_offer => false,
+                   :path_to_image => '/images/hawaiian.jpg'
+
+    Product.create :title => 'Pepperoni',
+                   :description => 'This is Pepperoni pizza',
+                   :price => 400,
+                   :size => 30,
+                   :is_spicy => false,
+                   :is_veg => false,
+                   :is_best_offer => false,
+                   :path_to_image => '/images/pepperoni.jpg'
+
+    Product.create :title => 'Vegetarian',
+                   :description => 'Amazing Vegetarian pizza',
+                   :price => 350,
+                   :size => 30,
+                   :is_spicy => false,
+                   :is_veg => true,
+                   :is_best_offer => false,
+                   :path_to_image => '/images/veg.jpg'
+
+  end
+end
